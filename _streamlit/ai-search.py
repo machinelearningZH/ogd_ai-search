bc4e5267acada9df3b9be3e66d06dda1854709f2
Patrick Arnecke <85753179+rnckp@users.noreply.github.com>
2025-04-09 07:03:49 +0200

    Update ai-search.py

--- _streamlit/ai-search.py
+++ _streamlit/ai-search.py
@@ -116,7 +116,7 @@ def log_interaction(start_time, raw_search_terms):
     """
     end_time = time.time()
     logging.warning(
-        f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\t{end_time-start_time:.3f}\t{raw_search_terms}'
+        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t{end_time - start_time:.3f}\t{raw_search_terms}"
     )
 
 
